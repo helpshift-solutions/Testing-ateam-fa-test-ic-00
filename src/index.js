d83366cea0ddbcf0d40853bb5b83c5c76dbfd6f9
Aarushi Sharma <aarushi.sharma@helpshift.com>
2025-08-29 15:59:20 +0530

    removed timee triggeR

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const { app } = require("@azure/functions");
 
 // Import your functions
 require("./functions/httpTrigger1")(app);
-require("./functions/timerTrigger")(app);
+// require("./functions/timerTrigger")(app);
